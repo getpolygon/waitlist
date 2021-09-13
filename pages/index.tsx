50093cfe06f0279016b8a420561bc225dd76fc2e
Michael <michael.grigoryan25@gmail.com>
2021-09-13 20:34:56 +0400

    Minor patch

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,25 +10,15 @@ import {
 } from "@chakra-ui/react";
 import Head from "next/head";
 import splitbee from "@splitbee/web";
+import type { NextPage } from "next";
 import { SiGoogleanalytics } from "react-icons/si";
-import type { GetStaticProps, NextPage } from "next";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const { SPLITBEE_TOKEN: token, NODE_ENV: env } = process.env;
+splitbee.init({
+  disableCookie: true,
+  token: process.env.NEXT_PUBLIC_SPLITBEE_TOKEN,
+});
 
-  if (env === "production") {
-    splitbee.init({
-      token,
-      disableCookie: true,
-    });
-
-    splitbee.track("Visits");
-  }
-
-  return {
-    props: {},
-  };
-};
+splitbee.track("Visits");
 
 const Home: NextPage = () => {
   return (
@@ -56,6 +46,7 @@ const Home: NextPage = () => {
         <Flex h={"full"} alignItems={"center"} justifyContent={"center"}>
           <Box
             p={4}
+            m={8}
             rounded={"2xl"}
             boxShadow={"xl"}
             userSelect={"none"}
@@ -63,59 +54,63 @@ const Home: NextPage = () => {
             bgColor={"gray.800"}
             borderColor={"purple.400"}
           >
-            <Center>
-              <Text
-                color={"purple.400"}
-                fontFamily={"ubuntu"}
-                fontSize={["4xl", "5xl", "6xl"]}
-              >
-                polygon
-              </Text>
-            </Center>
-
-            <Center>
-              <Box maxW={"lg"}>
+            <Stack spacing={4}>
+              <Center>
                 <Text
-                  textAlign={"center"}
+                  color={"purple.400"}
                   fontFamily={"ubuntu"}
-                  fontSize={["sm", "lg", "xl"]}
+                  fontSize={["4xl", "5xl", "6xl"]}
                 >
-                  <chakra.span color={"purple.400"}>New</chakra.span>,{" "}
-                  <chakra.span color={"purple.400"}>modern</chakra.span>, and{" "}
-                  <chakra.span color={"purple.400"}>private</chakra.span> social
-                  network that is not hungry for your data and personal life,
-                  coming soon.
+                  polygon
                 </Text>
-              </Box>
-            </Center>
+              </Center>
+
+              <Center>
+                <Box maxW={"lg"}>
+                  <Text
+                    textAlign={"center"}
+                    fontFamily={"ubuntu"}
+                    fontSize={["sm", "lg", "xl"]}
+                  >
+                    <chakra.span color={"purple.400"}>New</chakra.span>,{" "}
+                    <chakra.span color={"purple.400"}>modern</chakra.span>, and{" "}
+                    <chakra.span color={"purple.400"}>private</chakra.span>{" "}
+                    social network that is not hungry for your data and personal
+                    life, coming soon.
+                  </Text>
+                </Box>
+              </Center>
+
+              <Stack mt={4}>
+                <Center>
+                  <Link
+                    target={"_blank"}
+                    rel={"noreferrer noopener"}
+                    href={"https://app.splitbee.io/public/polygon.am"}
+                  >
+                    <Button
+                      size={"sm"}
+                      colorScheme={"yellow"}
+                      leftIcon={<SiGoogleanalytics />}
+                    >
+                      Splitbee analytics
+                    </Button>
+                  </Link>
+                </Center>
+
+                <Center userSelect={"none"}>
+                  <Text
+                    fontSize={"sm"}
+                    color={"gray.500"}
+                    fontWeight={"semibold"}
+                  >
+                    &copy; Polygon 2021
+                  </Text>
+                </Center>
+              </Stack>
+            </Stack>
           </Box>
         </Flex>
-
-        <Box mt={-36} pos={"relative"} bottom={0}>
-          <Stack spacing={4}>
-            <Center>
-              <Link
-                target={"_blank"}
-                rel={"noreferrer noopener"}
-                href={"https://app.splitbee.io/public/polygon.am"}
-              >
-                <Button
-                  size={"sm"}
-                  colorScheme={"yellow"}
-                  leftIcon={<SiGoogleanalytics />}
-                >
-                  Splitbee analytics
-                </Button>
-              </Link>
-            </Center>
-
-            <Center userSelect={"none"}>
-              <Text fontWeight={"semibold"} color={"gray.500"}>
-                &copy; Polygon 2021
-              </Text>
-            </Center>
-          </Stack>
-        </Box>
       </Box>
     </>
   );
