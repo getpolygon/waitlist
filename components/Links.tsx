5e451c6e98e316587dee1073f9698fb4d0eba993
Michael Grigoryan <56165400+michaelgrigoryan25@users.noreply.github.com>
2021-10-14 17:09:16 +0400

    Update Links.tsx

--- components/Links.tsx
+++ components/Links.tsx
@@ -29,7 +29,7 @@ const Links = () => {
   return (
     <Box>
       <Center>
-        <Stack direction={"row"}>
+        <Stack justifyContent={"center"} direction={["column", null, "row"]}>
           {links.map((link) => (
             <Link
               key={link.href}
