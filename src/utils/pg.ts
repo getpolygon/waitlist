457ca69c80918bc7f19a6e7a742bd8c79feb22a9
Michael Grigoryan <56165400+michaelgrigoryan25@users.noreply.github.com>
2021-12-14 17:17:36 +0400

    Minor patch

--- src/utils/pg.ts
+++ src/utils/pg.ts
@@ -1,7 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = process.env.POSTGRES_URL;
-const pg = new Pool({ connectionString });
+const pg = new Pool({ connectionString: process.env.POSTGRES_URL });
 pg.connect().catch(console.error);
 
 export default pg;
