0087fcdaf94d6695944020658ebbeff0a4b84588
michaelgrigoryan25 <56165400+michaelgrigoryan25@users.noreply.github.com>
2022-06-04 10:47:54 +0000

    fixed support for safari

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,7 @@ export default class _Document extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Syne:wght@500;600;800&amp;display=swap" />
-          <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;display=swap" />
+          <link type="text/css" rel="stylesheet" href="/fonts/styles.css" />
         </Head>
         <Main />
         <NextScript />
