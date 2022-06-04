acd09d90249089b03d784c3d85eb466db8aae5da
michaelgrigoryan25 <56165400+michaelgrigoryan25@users.noreply.github.com>
2022-06-04 10:37:07 +0000

    fix google fonts on ios

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,8 @@ export default class _Document extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Syne:wght@500;600;800&display=swap" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Syne:wght@500;600;800&display=swap" />
+          <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         </Head>
         <Main />
         <NextScript />
