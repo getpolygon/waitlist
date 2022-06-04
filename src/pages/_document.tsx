2f6d67d8ec0bd9bbd73ad6309daaeb5489ea2efb
michaelgrigoryan25 <56165400+michaelgrigoryan25@users.noreply.github.com>
2022-06-04 10:35:48 +0000

    minor patch

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,19 +6,9 @@ export default class _Document extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Syne:wght@500,800&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Syne:wght@500;600;800&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         </Head>
         <Main />
         <NextScript />
